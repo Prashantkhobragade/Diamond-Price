f644f3595316641f9afc2d35aa6c388072a9181f
prashant <prashantkhobragade011@gmail.com>
2024-01-12 11:55:39 +0530

    data ingestion started

---
+++ src/components/data_transformation.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+import numpy as np 
+from src.logger.logging import logging
+from src.exception.exeption import CustomException
+
+import os
+import sys 
+from dataclasses import dataclass
+from pathlib import Path
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
+@dataclass
+class DataTransformationConfig:
+    pass
+
+class DataTransformation:
+    def __init__(self):
+        pass
+    
+    def initiate_data_transformation(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise CustomException(e, sys)
+
+
