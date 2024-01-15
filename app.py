95b4fd656716fab68d9fadd02a010a4aacdc76d3
prashant <prashantkhobragade011@gmail.com>
2024-01-16 00:04:19 +0530

    basic flask app added

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from src.pipeline.prediction_pipeline import PredictPipeline, CustomData
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/predict", methods = ["GET", "POST"])
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template("from.html")
+    
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=8000)
