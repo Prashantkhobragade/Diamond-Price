d4f1f63f67874f4472e5cae63f97fd8ac8aab90f
prashant <prashantkhobragade011@gmail.com>
2024-01-16 00:11:34 +0530

    templates added for flask app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home_page():
 @app.route("/predict", methods = ["GET", "POST"])
 def predict_datapoint():
     if request.method=="GET":
-        return render_template("from.html")
+        return render_template("form.html")
     
 
 if __name__ =="__main__":
